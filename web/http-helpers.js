ea03c8e79cbc0873341d19c20ba7e2c314a56e80
Hack Reactor Student <student@hackreactor.com>
2017-11-01 15:25:09 -0700

    Passes first test, but poorly

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -14,6 +14,45 @@ exports.serveAssets = function(res, asset, callback) {
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...),
   // css, or anything that doesn't change often.)
+  
+  var headers = exports.headers;
+  
+  // This just gets the file type so we can set the header content-type properly
+  var extname = String(path.extname(asset.url)).toLowerCase();    
+  var mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpeg': 'image/jpg',
+    '.jpg': 'image/jpg',
+    '.gif': 'image/gif',
+    '.wav': 'audio/wav',
+    '.mp4': 'video/mp4',
+    '.woff': 'application/font-woff',
+    '.ttf': 'application/font-ttf',
+    '.eot': 'application/vnd.ms-fontobject',
+    '.otf': 'application/font-otf',
+    '.svg': 'application/image/svg+xml'
+  };
+  var contentType = mimeTypes[extname];
+  
+  
+  var filepath = __dirname + '/public' + asset.url;
+  console.log('filepath', filepath);
+  if (asset.url === '/') {
+    filepath += 'index.html';
+  }
+  
+  fs.readFile(filepath, function(err, data) {
+    if (err) { console.log(err); }
+    headers['Content-Type'] = contentType;
+    res.writeHead(200, headers);
+    res.write(data);
+    res.end(); // if we put something in the end() its basically writing it
+  });
+  
 };
 
 
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,14 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var httpHelpers = require('./http-helpers.js');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  
+  if (req.url !== '/favicon.ico') {
+    httpHelpers.serveAssets(res, req);  
+  }
+  
+  
+  // res.end(archive.paths.list);
 };
